cd7bd3d06ec3943b895fb7ecc44d86e8333df660
ACuteWoof <sutharshanvithushan@gmail.com>
2022-03-05 22:36:36 +0530

    Playlist card and playlist listing from local storage

--- src/Components/PlaylistCard/index.jsx
+++ src/Components/PlaylistCard/index.jsx
@@ -1,13 +1,20 @@
 import "./index.css";
 
-import { getLocalPlaylists } from "../../Functions/localPlaylists";
-
 function PlaylistCard(props) {
-  const localPlaylists = getLocalPlaylists();
-  // Just to do stuff with it before making the cards
-  console.log(localPlaylists);
-
-  return <></>;
+  const { id, name, thumbnail, isLocal, description } = props;
+  return (
+    <div className='PlaylistCard'>
+      <div className='PlaylistCardThumbnailDiv'>
+        <img src={thumbnail} alt={name} />
+      </div>
+      <div className='PlaylistCardInfo'>
+        <h3 className='PlaylistCardName'>{name}</h3>
+        <div className='PlaylistDescription'>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PlaylistCard;
--- src/Pages/PlaylistsPage/index.jsx
+++ src/Pages/PlaylistsPage/index.jsx
@@ -1,11 +1,32 @@
 import "./index.css";
 import PlaylistCard from "../../Components/PlaylistCard";
+import { getLocalPlaylists } from "../../Functions/localPlaylists";
 
 console.log(localStorage);
 
 function PlayslitsPage(props) {
-  const localPlaylists = localStorage.getItem("Playlists");
-  return <div className='PlaylistsPage'></div>;
+  const localPlaylists = getLocalPlaylists();
+  // Just to do stuff with it before making the cards
+  console.log(localPlaylists);
+
+  return (
+    <div className='PlaylistsPage'>
+      <h1>Playlists</h1>
+      <div className='PlaylistsGrid'>
+        {localPlaylists &&
+          localPlaylists.map((playlist) => (
+            <PlaylistCard
+              key={playlist.id}
+              id={playlist.id}
+              name={playlist.name}
+              thumbnail={playlist.thumbnail}
+              isLocal={true}
+              description={playlist.description}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default PlayslitsPage;
