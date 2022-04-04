796db57469cb2310973c7832086bd367e2f11ef5
ACuteWoof <sutharshanvithushan@gmail.com>
2022-04-04 21:57:43 +0530

    revert cors

--- src/REST/Itunes/index.js
+++ src/REST/Itunes/index.js
@@ -1,9 +1,9 @@
 const apiUrl = "https://itunes.apple.com";
-// const cors = "https://cors-woof.herokuapp.com";
+const cors = "https://cors-woof.herokuapp.com";
 
 function musicSearch(terms, limit) {
   return fetch(
-    `${apiUrl}/search?limit=${limit}&media=music&entity=musicTrack&term=${terms}`
+    `${cors}/${apiUrl}/search?limit=${limit}&media=music&entity=musicTrack&term=${terms}`
   )
     .then((response) => response.json())
     .catch((error) => console.log(error));
--- src/REST/YouTube/index.js
+++ src/REST/YouTube/index.js
@@ -1,8 +1,8 @@
 const apiUrl = "https://woof-yt-scrape.herokuapp.com/api";
-// const cors = "https://cors-woof.herokuapp.com";
+const cors = "https://cors-woof.herokuapp.com";
 
 async function searchYouTube(terms) {
-  let r = await fetch(`${apiUrl}/search?q=${terms}`);
+  let r = await fetch(`${cors}/${apiUrl}/search?q=${terms}`);
   let data = await r.json();
   return data;
 }
