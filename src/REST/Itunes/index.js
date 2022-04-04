9f9ebcfd49e2ae0978bd174eb22402a663e3afd2
ACuteWoof <sutharshanvithushan@gmail.com>
2022-04-04 21:55:35 +0530

    remove cors

--- src/REST/Itunes/index.js
+++ src/REST/Itunes/index.js
@@ -1,9 +1,9 @@
 const apiUrl = "https://itunes.apple.com";
-const cors = "https://cors-woof.herokuapp.com";
+// const cors = "https://cors-woof.herokuapp.com";
 
 function musicSearch(terms, limit) {
   return fetch(
-    `${cors}/${apiUrl}/search?limit=${limit}&media=music&entity=musicTrack&term=${terms}`
+    `${apiUrl}/search?limit=${limit}&media=music&entity=musicTrack&term=${terms}`
   )
     .then((response) => response.json())
     .catch((error) => console.log(error));
